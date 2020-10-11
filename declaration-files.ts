e6f81d0d95d179565642e8b4856b419b553f6c46
nico <410827024@qq.com>
2020-10-12 00:46:54 +0800

    declare

--- declaration-files.ts
+++ declaration-files.ts
@@ -1,3 +1,7 @@
 import { Action } from 'redux'
-declare var jQuery:(selector: string) => any;
-jQuery('#id'); // 自己一个个写d.ts很累，一般社区会有很多types https://github.com/DefinitelyTyped/DefinitelyTyped  这个组织管理者很多types
+// declare var jQuery:(selector: string) => any;
+// 自己一个个写d.ts很累，一般社区会有很多types https://github.com/DefinitelyTyped/DefinitelyTyped  这个组织管理者很多types
+// 或者很多库是直接提供了定义文件和源代码 比如redux
+// typescript 是怎么知道这些types定义文件的
+// 声明文件也就是declare的组合
+jQuery('#id');
