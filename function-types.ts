befeeaa9d92012d987d1fe703d9688337c3269e0
nico <410827024@qq.com>
2020-10-13 00:15:20 +0800

    function

--- function-types.ts
+++ function-types.ts
@@ -9,3 +9,11 @@ interface ISum {
   (x: number, y: number, z?: number): number
 }
 let add2: ISum = add
+
+function add1(x: number, y: number, z?: number): number {
+  if (typeof z === 'number') {
+    return x + y + z;
+  } else {
+    return x + y;
+  }
+}
