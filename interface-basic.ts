ff27c801da6a3be28405bc88c18fe57d2e1dc054
nico <410827024@qq.com>
2020-10-13 00:11:24 +0800

    function type

--- interface-basic.ts
+++ interface-basic.ts
@@ -10,3 +10,14 @@ let viking: Person = {
   name: 'viking',
   age: 20,
 }
+
+
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function (source: string, subString: string): boolean {
+  let result = source.search(subString);
+  return result >= -1;
+};
