48c61e2661e99f8fcbfb45fa575a76449cadfe6e
nico <410827024@qq.com>
2020-10-13 00:24:01 +0800

    类型推论 类型断言 类型守卫 联合类型

--- type-inference-and-more.ts
+++ type-inference-and-more.ts
@@ -1,22 +1,22 @@
 // type inference
-let str = 'str'
+let str = 'str' // 类型推论
 
 // union types
-let numberOrString: number | string
+let numberOrString: number | string // 联合类型
 
 function getLength(input: string | number): number {
   const str = input as string
   if (str.length) {
     return str.length
   } else {
-    const number = input as number
+    const number = input as number; // 类型断言
     return number.toString().length
   }
 }
 
 //type guard
 function getLength2(input: string | number): number {
-  if (typeof input === 'string') {
+  if (typeof input === 'string') { // 类型守卫
     return input.length
   } else {
     return input.toString().length
