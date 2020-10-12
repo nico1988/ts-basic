51980756e3ac4a07447990332a25fd6ae6522010
nico <410827024@qq.com>
2020-10-12 23:30:45 +0800

    元组

--- array-and-tuple.ts
+++ array-and-tuple.ts
@@ -2,7 +2,7 @@ let arrOfNumbers: number[] = [1,2,3]
 arrOfNumbers.push(3)
 
 function test() {
-  console.log(arguments)
+  console.log(arguments); // IArguments
 }
 
-let user: [string, number] = ['viking', 20]
+let user: [string, number] = ['viking', 20]; // 元组起源于函数式编程 python中也有 就是不同类型的数据在数组里  如果用any就丧失了
